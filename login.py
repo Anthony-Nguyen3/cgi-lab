f0ad4b241dbdb11c5f0eef229ae38f37f057936a
Anthony Nguyen <anthony3@ualberta.ca>
2022-09-26 17:29:38 -0600

    Added files

---
+++ login.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from http import cookies
+import os
+import cgi
+from templates import login_pag
+from templates import secret_page
+
+def parse_cookies(cookie_string):
+    cookies
+
